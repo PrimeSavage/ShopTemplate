3a456bf9a1648fe5bfc52df1b8e6da3e0c6f8c43
PrimeSavage <pr1mes4v4ge2000@gmail.com>
2021-08-03 10:08:42 +0300

    main commit

---
+++ categoryPage.php
@@ -0,0 +1,132 @@
+<html>
+    <head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<meta charset="utf-8">
+        <title>Bianco</title>
+    </head>
+	<body>
+		<?php include 'header.html'; ?>
+	  <div class="container categoryImg">
+	    <img style="width: 100%" src="https://avatars.mds.yandex.net/get-pdb/752643/968aa6a1-2afe-487e-86cb-7ac8f493b324/s1200">
+	  </div>
+	  <div class="container mainCategory">
+		<section class="filter col-3">
+		  <h4>GENDER</h4>
+		    <div class="form-check">
+			  <input class="form-check-input" type="checkbox">
+			  <label>
+				MEN
+			  </label>
+			</div>
+			<div class="form-check">
+			  <input class="form-check-input" type="checkbox">
+			  <label>
+				WOMEN
+			  </label>
+			</div>
+		</section>
+		<section class="catalogue col">
+		  <div class="card-columns">
+		    <div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale" style="visibility: hidden">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale"  style="visibility: hidden">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale"  style="visibility: hidden">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+			<div class="card">
+			  <div class="card-body card-body-top">
+				<p class="card-text sale" style="visibility: hidden">SALE: 50%</p>
+				<img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+			  </div>
+			  <a href="productPage.php"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+			  <div class="card-body card-body-bottom">
+				<p>#Name#</p>
+				<p>#BC#</p>
+				<p>#Price#</p>
+			  </div>
+			</div>
+		  </div>
+		</section>
+	  </div>
+		<?php include 'footer.html'; ?>
+	</body>
+</html>
---
+++ productPage.php
@@ -0,0 +1,50 @@
+<html>
+    <head>
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<meta charset="utf-8">
+        <title>Bianco</title>
+    </head>
+	<body>
+		<?php include 'header.html'; ?>
+		<div class="container">
+			<div class="breadcrumb-nav">
+			  <ol class="breadcrumb">
+				<li class="breadcrumb-item"><a href="categoryPage.php">Back</a></li>
+				<li class="breadcrumb-item"><a href="categoryPage.php">Stock collection</a></li>
+				<li class="breadcrumb-item active">Item</li>
+			  </ol>
+			</div>
+			<div class="product-main row">
+			  <div class="col-7 item-img">
+			   <div class="row">
+				<div class="col-3">
+				  <a href="#"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+				  <a href="#"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+				  <a href="#"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+				</div>
+				<div class="col">
+				  <a href="#"><img src="https://s.w.org/style/images/about/WordPress-logotype-simplified.png"></a>
+				</div>
+			   </div>
+			  </div>
+			  <div class="col">
+				<div class="order-top">
+				  <p>SALE: 50%</p>
+				  <img class="heart" src="https://img.icons8.com/ios/452/like--v1.png">
+				</div>
+				<h4>BEND NATURAL LEATHER</h4>
+				<p>#BC Number#</p>
+				<p class="font-weight-bold">#WSP RSP#</p>
+				<hr>
+				<h6>Color selection</h6>
+				<hr>
+				<a class="btn btn-link-light" href="#" style="padding: 0">More product information</a>
+				<hr>
+				<button type="button" class="btn btn-outline-secondary" style="width: 100%">GO TO ORDER GRID</button>
+			  </div>
+			</div>
+		</div>
+		<?php include 'footer.html'; ?>
+	</body>
+</html>
